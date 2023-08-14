5ac0448b136971cb9538bce6a6c824751323cbaf
jace <nic.chiron@gmail.com>
2023-08-14 11:33:45 +0200

    install jotai

---
+++ src/atoms/atoms.js
@@ -0,0 +1,7 @@
+import { atom } from 'jotai';
+
+export const tokenAtom = atom(null);
+export const descriptionAtom = atom(null);
+export const idAtom = atom(null);
+export const emailAtom = atom(null);
+export const usernameAtom = atom(null);
